f278181d2b3a0f8c351334d4794c6a19c3bc0148
Zoljargal Fallows <zol.fallows@hotmail.com>
2019-01-14 20:28:25 +0000

    changed JS function file

--- function.js
+++ function.js
@@ -1,6 +1,27 @@
 function createGreeting(name, age){
-    return 'Hi, my name is Matt and I\'m 25';
+    return `Hello, My name is ${name}, I am ${age} years old.`;
 }
 
-const greeting1 = createGreeting();
-console.log(greeting1);
+function getYearOfBirth(age){
+    let yearOfBirth = 2019 - age;
+    if (typeof age !== "number"){
+        throw new Error("Age must be a number");
+    } else if (age < 0) {
+        throw new Error("Age can not be negative")
+    } else {
+    return `I was born in ${yearOfBirth}`;
+    }
+}
+
+try {
+    const greeting1 = createGreeting();
+    console.log(greeting1);
+} catch(e) {
+    console.log(e);
+}
+
+// const greeting1 = createGreeting();
+// console.log(greeting1);
+
+createGreeting("John", 25);
+getYearOfBirth(12);
