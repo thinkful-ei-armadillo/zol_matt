9f6cce85aca4e6c387351618747357b4f73f7f18
matth3wn <matth3wn@gmail.com>
2019-01-14 13:54:16 -0800

    functions 2

--- function.js
+++ function.js
@@ -3,25 +3,25 @@
 // function drill 1
 
 function createGreeting(name, age){
-    return `Hello, My name is ${name}, I am ${age} years old.`;
+  return `Hello, My name is ${name}, I am ${age} years old.`;
 }
 
 function getYearOfBirth(age){
   let yearOfBirth = 2019 - age;
   if (typeof age !== "number"){
-        throw new Error("Age must be a number");
-    } else if (age < 0) {
-        throw new Error("Age can not be negative")
-    } else {
+    throw new Error("Age must be a number");
+  } else if (age < 0) {
+    throw new Error("Age can not be negative")
+  } else {
     return `I was born in ${yearOfBirth}`;
-    }
+  }
 }
 
 try {
-    const greeting1 = getYearOfBirth('-1');
-    console.log(greeting1);
+  const greeting1 = getYearOfBirth('-1');
+  console.log(greeting1);
 } catch(e) {
-    console.log(e);
+  console.log(e);
 }
 
 createGreeting("John", 25);
@@ -37,37 +37,37 @@ console.log(jediName('matt','smith'));
 
 function beyond(num)
 {
-    if (num === Infinity || num === Number.NEGATIVE_INFINITY ){
-        console.log("And beyond");
-    } else if (num > 0 && num < Infinity){
-        console.log("To infinity");
-    } else if (num < 0 && num > Number.NEGATIVE_INFINITY){
-        console.log("To negative infinity");
-    } else {
-        console.log("Staying home");
-    }
+  if (num === Infinity || num === Number.NEGATIVE_INFINITY ){
+    console.log("And beyond");
+  } else if (num > 0 && num < Infinity){
+    console.log("To infinity");
+  } else if (num < 0 && num > Number.NEGATIVE_INFINITY){
+    console.log("To negative infinity");
+  } else {
+    console.log("Staying home");
+  }
 }
 
 console.log(beyond(2));
 
 function decode(code){
-    let string1 = code.split(' ');
-    let newArr = [];
-    let space = ' ';
-    
-    for(let i = 0; i < string1.length; i++){
-        if(string1[i][0] === 'a' )
-            newArr.push(string1[i][1]); 
-        if(string1[i][0] === 'b' )
-            newArr.push(string1[i][2]);
-        if(string1[i][0] === 'c' )
-            newArr.push(string1[i][3]);
-        if(string1[i][0] === 'd' )
-            newArr.push(string1[i][4]);
-        else    
-            newArr.push(space);            
-    }    
-    return newArr.join(' ');
+  let string1 = code.split(' ');
+  let newString = '';
+  let space = ' ';
+
+  for(let i = 0; i < string1.length; i++){
+    if(string1[i][0] === 'a')
+      newString+=string1[i][1]; 
+    if(string1[i][0] === 'b' )
+      newString+=string1[i][2];
+    if(string1[i][0] === 'c' )
+      newString+=string1[i][3];
+    if(string1[i][0] === 'd' )
+      newString+=string1[i][4];
+    else    
+      newString+=space;            
+  }    
+  return newString;
 }
 
 console.log(decode('craft block argon meter bells brown croon droop'));
